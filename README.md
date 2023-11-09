fa3ef754bb5636e4cb4ddf3963cca0416133f518
Baptiste Wasson <baptiste.wasson@gmail.com>
2023-11-09 09:10:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jeu-de-l-oie
---
+++ src/main/java/fr/wasson/App.java
@@ -0,0 +1,46 @@
+package fr.wasson;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Random generateur = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int nbCases;
+        int nbLances;
+        int lancer;
+        int reste;
+        int numCase;
+
+        do {
+            nbCases = 20;
+            nbLances = 5;
+            lancer = 0;
+            reste = nbCases;
+            numCase = 0;
+
+            for (int i = 1; i <= nbLances; i++) {
+                lancer = generateur.nextInt(6) + 1;
+                reste = reste - lancer;
+                numCase = numCase + lancer;
+                System.out.print("Lancer " + i + " : vous avez fait " + lancer + ". Vous êtes sur la case " + numCase + " (encore " + reste + " cases)");
+                scanner.nextLine();
+            };
+
+            if (numCase == 20) {
+                System.out.println("Vous avez gagné !");
+            } else {
+                System.out.println("Vous avez perdu !");
+            }
+
+        } while (numCase != 20);
+
+        scanner.close();
+
+    }
+    
+}
